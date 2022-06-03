dd7ed26e07da8e6b4933302981c85eabcb2f8f98
Justin Williams <justinmorganwilliams@gmail.com>
2022-06-03 03:03:33 -0400

    updated app with variable wxh for map

--- nyc_covid_tracker_app/app.R
+++ nyc_covid_tracker_app/app.R
@@ -9,52 +9,95 @@
 
 library(shiny)
 library(tidyverse)
+library(sf)
 library(leaflet)
 library(htmlwidgets)
 library(BAMMtools)
 library(htmltools)
+library(scales)
 
+# set working directory
 setwd("/Users/justinwilliams/projects/nyc_covid_tracker/nyc_covid_tracker_app/")
+
+# read in df's
 all_sf <- readRDS("all_sf.rds")
+summary_totals <- readRDS("summary_totals.Rds")
+casesbyday_cum <- as.data.frame(readRDS("casesbyday_cum.Rds"))
 
 # define map start point
 start_point <- as.Date("2022-01-01",
                        format = "%Y-%m-%d")
 
+# obtain summary numbers
+#case count
+case_count <- 
+  summary_totals[summary_totals$MEASURE == 
+                   "NYC_TOTAL_CASE_COUNT",]$NUMBER_OF_NYC_RESIDENTS
+
+# death count
+death_count <- 
+  summary_totals[summary_totals$MEASURE == 
+                   "NYC_TOTAL_DEATH_COUNT",]$NUMBER_OF_NYC_RESIDENTS
+
+# date
+current_date <- 
+  summary_totals[summary_totals$MEASURE == 
+                   "DATE_UPDATED",]$NUMBER_OF_NYC_RESIDENTS
+
 # Define UI for application 
 ui <- fluidPage(
-
+  
     # Application title
-    titlePanel("Covid-19 NYC Trends by Modified ZCTA 
-               (August 2020 - present)"),
-    # Sidebar with date input
-    sidebarLayout(
-        sidebarPanel(
-          tags$a(href="https://github.com/nychealth/coronavirus-data",
-                 "Data Repository", target="_blank"),
-          h5("Data is aggregated by week, seperated by week ending date.
-             All data sourced from the NYC Department of Health."),
-            sliderInput(inputId = "date",
-                        label = "Select a date (week ending in):",
-                        min = as.Date(min(all_sf$week_ending)),
-                        max = as.Date(max(all_sf$week_ending)),
-                        value = start_point,
-                        step = 7,
-                        timeFormat = "%b %d-'%y"
-                        # choices = unique(all_sf$week_ending)
-            )
+    titlePanel("COVID-19 NYC Tracker by Zip Code"),
+    
+    fluidRow(
+      column(4,
+          span(tags$i(h6("All data sourced from the NYC Department of Health\n
+                            and Mental Hygiene (DOHMOH) ",
+                    a(href="https://github.com/nychealth/coronavirus-data",
+                                       "data repository.", target="_blank")))),
+          span(h3(paste0(prettyNum(case_count, big.mark = ",")," cases")), 
+             align = "right",style="color:#045a8d"),
+          span(h4(paste0(prettyNum(death_count, big.mark = ",")," deaths")),  
+             align = "right", style="color:#045a8d"),
+          span(h6(prettyNum(current_date)), align = "right"),
+          plotOutput("case_count", height="200px", width="100%"),
+          plotOutput("cumulative", height="200px", width="100%"),
+          sliderInput(inputId = "date",
+                    label = "Select mapping date (week ending in):",
+                    min = as.Date(min(all_sf$week_ending)),
+                    max = as.Date(max(all_sf$week_ending)),
+                    value = start_point,
+                    step = 7,
+                    timeFormat = "%b %d-'%y",
+                    width = "100%",
+                    animate = 
+                      animationOptions(interval = 3000,
+                                       playButton = c("Play"),
+                                       pauseButton = c("Pause"),
+                                       loop = T)),
+span(tags$i(h6("*Mapping data is aggregated by week, seperated by week ending 
+               date and begins August 2020"))),
         ),
         # define panels
+      column(width =  8,
         mainPanel(
           tabsetPanel(
-            tabPanel("Case Rate", leafletOutput(outputId = "cases",
-                                                height = 450)),
-            tabPanel("Test Rate", leafletOutput(outputId = "tests",
-                                                height = 450)),
-            tabPanel("Percent Positive", leafletOutput(outputId = "pctpos",
-                                                       height = 450))
+            tabPanel("Case Rate", 
+                     column(12,leafletOutput(outputId = "cases",
+                                             height = "80vh",
+                                             width = "60vw"))),
+            tabPanel("Test Rate", 
+                     column(12,leafletOutput(outputId = "tests",
+                                            height = "80vh",
+                                             width = "60vw"))),
+            tabPanel("Percent Positive", 
+                     column(12,leafletOutput(outputId = "pctpos",
+                                             height = "80vh",
+                                             width = "60vw")))
           )
         )
+      )
     )
 )
 # Define server logic required to draw maps
@@ -64,10 +107,38 @@ server <- function(input, output) {
       w <- all_sf %>% filter(week_ending == input$date)
       return(w)
     })
+    
+  output$case_count <- renderPlot({
+    (case_count_plot <-  casesbyday_cum %>% 
+      ggplot(aes(date, case_count)) +
+      geom_bar(stat = "identity", alpha = 0.8) +
+      scale_y_continuous(labels = number_format(scale = .0001,
+                                                suffix = "k")) +
+      scale_x_continuous(labels = NULL) +
+      labs(y = "case count", x = "") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1),
+            axis.text = element_text(size = 8)))
+  }, res = 96)
+  
+  output$cumulative <- renderPlot({
+    (casesbyday_cum_plot <- casesbyday_cum %>% 
+      ggplot(aes(date, case_cum)) +
+      geom_line(lwd = 2, alpha = 0.8) +
+      scale_y_continuous(labels = number_format(scale = .0001,
+                                                suffix = "k")) +
+      scale_x_date(date_breaks = "2 month",
+                   date_labels = "%b-'%y") +
+      labs(y = "cumulative cases", x = "") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1),
+            axis.text = element_text(size = 8)))
+    
+  }, res = 96)
 
-  output$cases <- renderLeaflet({
+  output$cases <- renderLeaflet( {
     # set bins
-    bins <- getJenksBreaks(all_sf$caserate, 9) # get natural breaks
+    bins <- getJenksBreaks(all_sf$caserate, 10) # get natural breaks
     bins <- map(.x = bins, round) %>% 
       unlist() # round
     
@@ -109,7 +180,7 @@ server <- function(input, output) {
   
   output$tests <- renderLeaflet({
     # set bins
-    bins <- getJenksBreaks(all_sf$testrate, 9) # get natural breaks
+    bins <- getJenksBreaks(all_sf$testrate, 10) # get natural breaks
     bins <- map(.x = bins, round) %>%
       unlist()# round
     
@@ -151,7 +222,7 @@ server <- function(input, output) {
   
   output$pctpos <- renderLeaflet({
     # set bins
-    bins <- getJenksBreaks(all_sf$percpos, 9) # get natural breaks
+    bins <- getJenksBreaks(all_sf$percpos, 10) # get natural breaks
     bins <- map(.x = bins, round) %>% 
       unlist() # round
     
